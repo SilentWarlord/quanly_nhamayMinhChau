5819cfb81b4c6391b1235b12949379dc54d544d0
DangPhungThienAn <anbum7733@gmail.com>
2024-05-06 00:03:19 +0700

    login

---
+++ quanly_nhamayMinhChau/Models/NguyenLieu.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace quanly_nhamayMinhChau.Models
+{
+    internal class NguyenLieu
+    {
+        public string maNL {  get; set; }
+        public string tenNL { get; set; }
+        public string DVD { get; set; }
+        public int giaThanh {  get; set; }
+        public DateTime ngayNhap { get; set; }
+
+    }
+}
